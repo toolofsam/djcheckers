11e71f5d8149f0163ac19daca40bc7c334079fdb
Mike Lopez <dev@scidentify.info>
2015-06-19 18:12:52 -0400

    comment on move position numbers

--- checkers_gameboard/gameboard/move.py
+++ checkers_gameboard/gameboard/move.py
@@ -11,6 +11,12 @@ def where_can_i_go(grid, n):
 
     # start with a empty list
     around_me = []
+
+    # these numbers should be a bit smarter
+    #and should be calculated from the max blocks in the gameboard
+    # these numbers are hardcoded for a 8x8 gameboard and will need 
+    # to optimize if you want to play on a dynamic board.
+
     l = [-1, -9, -8, -7, 1, 7, 8, 9]
     for i in l:
         if not n+i > settings.per_row ** 2 and not n+i < int(0):
